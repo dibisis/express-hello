c7a5cf86385cfefadd288219ff6c354abc500555
dibisis <dibisis@gmail.com>
2020-08-09 10:23:20 +0900

    sample for pipeline trigger

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express()
 const port = 8080
-
+# sample for pipe line excution
 app.get('/', (req, res) => res.send('Hello World!\n'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
